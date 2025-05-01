fe227c356a811a999161b3697056a4ee9fc6bd01
RelixOfficial <relixofficial4@gmail.com>
2025-05-01 19:34:05 +0700

    Create run.js

---
+++ run.js
@@ -0,0 +1,25 @@
+const { spawn } = require('child_process');
+process.env.TZ = 'Asia/Jakarta';
+console.log('Welcome to your server!\nType your command and send it to your server on "type a command..."')
+
+/**
+ * Function to start a command process.
+ * @param {string} cmd - The command to execute.
+ */
+function start(cmd) {
+    try {
+        // Spawn the desired command process
+        const childProcess = spawn(cmd, [], {
+            stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+        });
+
+        // Handle errors
+        childProcess.on('error', (error) => {
+            console.error('Error starting process:', error.message);
+        });
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+start('bash');
